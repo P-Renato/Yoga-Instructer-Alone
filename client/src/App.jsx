c1ff257afc66cbb7c2869ef34ce928a487ddd42e
Renato <paulo.borupdebakker@dci-student.org>
2025-07-03 16:44:31 +0200

    update with blog page fixed and dynamic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import "./App.css";
 import Header from "./Header";
-import AboutMe from './AboutMe';
-import Schedule from './Schedule';
-import Blog from './Blog';
+import AboutMe from './pages/AboutMe';
+import Schedule from './pages/Schedule';
+import Blog from './pages/Blog';
 
 function App() {
   // const [count, setCount] = useState(0)
